e2846c3a72c490de0c1e52bdd4e15c25540ed486
gustavomaes <gustavomaescd@gmail.com>
2019-02-17 07:21:35 -0300

    setup server and login system (based on fotontech/gigatron)

---
+++ server/src/modules/user/index.ts
@@ -0,0 +1,80 @@
+import UserType from './UserType';
+import * as Loader from './UserLoader';
+import createConnection from '../utils/createCoonnection';
+import { ForbiddenError } from 'apollo-server';
+
+import { GraphQLNonNull, GraphQLID, GraphQLInt, GraphQLString, GraphQLInputObjectType } from 'graphql';
+
+const isAuthenticated = (me, resolver) => me ? resolver : new Error('Not authenticated')
+
+export const queries = {
+  me: {
+    type: UserType,
+    resolve:  (object, args, { me }) => isAuthenticated(me, me)
+  },
+  users: {
+    type: createConnection(UserType, 'UserConnection'),
+    args: {
+      size: {
+        type: GraphQLNonNull(GraphQLInt),
+      },
+      page: {
+        type: GraphQLNonNull(GraphQLInt),
+      }
+    },
+    resolve: (object, args, { me }) => isAuthenticated(me, Loader.Users(object, args, { me }))
+  },
+  user: {
+    type: UserType,
+    args: {
+      id: {
+        type: GraphQLNonNull(GraphQLID),
+      },
+    },
+    resolve: (object, args, { me }) => isAuthenticated(me, Loader.User(object, args, { me }))
+  }
+}
+
+export const mutations = {
+  addUser: {
+    type: UserType,
+    args: {
+      input: {
+        type: new GraphQLInputObjectType({
+          name: 'AddUserInput',
+          fields: () => ({
+            name: {
+              type: (GraphQLString)
+            },
+            email: {
+              type: (GraphQLString)
+            },
+            password: {
+              type: (GraphQLString)
+            },
+          }),
+        })
+      }
+    },
+    resolve: Loader.AddUser
+  },
+  loginUser: {
+    type: UserType,
+    args: {
+      input: {
+        type: new GraphQLInputObjectType({
+          name: 'LoginUserInput',
+          fields: () => ({
+            email: {
+              type: (GraphQLString)
+            },
+            password: {
+              type: (GraphQLString)
+            },
+          }),
+        })
+      }
+    },
+    resolve: Loader.LoginUser
+  }
+}
